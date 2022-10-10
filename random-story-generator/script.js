0a1a3fb8983420148edc5451ca0e03eaae1cc8ed
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-10 12:08:51 -0700

    create var arrayOfStory

--- random-story-generator/script.js
+++ random-story-generator/script.js
@@ -11,3 +11,13 @@ change <p> text content to randomly generated story
 
 let btn = document.getElementById('btn')
 let story = document.getElementsByClassName('random-story')[0]
+let arrayOfStory = ["The bubbly bubbles came back after the clowns resurrected him in an ancient burial spell of magicalness.",
+"The elephant had a pig friend who taught math. The pig exploded one day and left the whole world wondering.",
+"The gorillas started causing a ruckus in The Big Apple, actually a big apple, where they ran over Billy Joe and Eileen.",
+"Once upon a time there lived a girl named Kate. Kate lived on a hill, and one day Kate went for a walk with her sword.",
+"The flying fish swim from the bottom of the ocean then jump out of the water and touch the sky",
+"When they got to the soup kitchen, they stared in horror for a few moments, then turned into a slug and crawled away",
+"There is also a donut store where they sell burgers, and a check cashing store in the small strip mall, but everybody references the market when talking about the Phone.",
+"Something happened in the lab the other night. I\’m not certain what but it was something.I powered up the device, and there she was. It was a possibility I hadn\’t considered.",
+"Your eyes pour the moonlight on my path as I turn my back to you. In the dead of the night, I walk with my family, away from my home, my street, my town, and what was till yesterday- my country.",
+"Zelina walked in the middle of the train track. She and her sister used to lay in bed listening for the comforting whistle of the train that passed at three o\’clock every morning."]
