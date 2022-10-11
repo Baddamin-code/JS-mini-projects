55ddf27606738968631d7ebdc8a9a689b5db2022
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-11 08:39:10 -0700

    set initial count

--- basic-counter/alt.js
+++ basic-counter/alt.js
@@ -1,6 +1,10 @@
 /*
+set initial count
  grab .count from alt.html
 grab buttons
 */
-let count = document.querySelector('.count')
-let btns = document.querySelectorAll('button')
+let count = 0;
+let value = document.querySelector(".count");
+let btns = document.querySelectorAll("#btn");
+
+
