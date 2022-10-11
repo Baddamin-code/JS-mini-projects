421ffdd9dc0e45420e9a59469044c0e2a587d13a
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-11 08:51:53 -0700

    add event listener to listen for button click

--- basic-counter/alt.js
+++ basic-counter/alt.js
@@ -7,7 +7,10 @@ let count = 0;
 let value = document.querySelector(".count");
 let btns = document.querySelectorAll(".btn");
 
-// create forEach function to select each button
+// create for each function to select each button
 btns.forEach(function (btn) {
+    // add event listener to listen for button click
+    btn.addEventListener('click', function(e) {
 
+    })
 })
