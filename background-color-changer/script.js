4c668d03bb396b5f4bfdc882ea32d020c0b5eadf
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-11 01:00:15 -0700

    comment code

--- background-color-changer/script.js
+++ background-color-changer/script.js
@@ -1,9 +1,11 @@
 /*
 #007bff, #6610f2, #6f42c1, #e83e8c;#dc3545, #fd7e14, #ffc107, #28a745, #20c997, #17a2b8, #fff, #6c757d, #343a40, #007bff, #6c757d, #28a745, #17a2b8, #ffc107, #dc3545, #f8f9fa, #343a40
 */
-
+// get div container
 let cont = document.getElementById("container");
+//  get html button
 let btn = document.getElementById("btn");
+// array of colors
 let colors = [
   "#007bff",
   "#6610f2",
