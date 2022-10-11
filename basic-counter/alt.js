3ef0284c117c7322324842b99b4643292e9631eb
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-11 08:57:33 -0700

    reset value

--- basic-counter/alt.js
+++ basic-counter/alt.js
@@ -9,16 +9,20 @@ let btns = document.querySelectorAll(".btn");
 
 // create for each function to select each button
 btns.forEach(function (btn) {
-    // add event listener to listen for button click
-    btn.addEventListener('click', function(e) {
-        // create a var that targets each button through the class name
-        const clickBtn = e.currentTarget.classList;
-        // if statement to increase value
-        if (clickBtn.contains("increase")){
-            count++;
-        } // if statement to decrease value
-        else if (clickBtn.contains("decrease")) {
-            count--;
-        }
-    })
-})
+  // add event listener to listen for button click
+  btn.addEventListener("click", function (e) {
+    // create a var that targets each button through the class name
+    const clickBtn = e.currentTarget.classList;
+    // if statement to increase value
+    if (clickBtn.contains("increase")) {
+      count++;
+    } // if statement to decrease value
+    else if (clickBtn.contains("decrease")) {
+      count--;
+    } // reset value
+    else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
