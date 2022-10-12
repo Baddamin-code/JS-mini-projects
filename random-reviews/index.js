c877092a143a34f532e53563ba95fa22f8a7fe25
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-12 01:46:25 -0700

    grab image paths

---
+++ random-reviews/index.js
@@ -0,0 +1,9 @@
+// let img = document.querySelector("img")
+// img.setAttribute('src', img1)
+
+// grab image paths
+const img1 = "img/avatar2.jpg"
+const img2 = "img/avatar3.jpg"
+const img3 = "img/avatar4.jpg"
+const img4 = "img/avatar5.jpg"
+
