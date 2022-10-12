dc32bd3505b384c12fc8129d93cfc4e6227e2be6
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-12 01:54:59 -0700

    add review data

--- random-reviews/index.js
+++ random-reviews/index.js
@@ -7,3 +7,39 @@ const img2 = "img/avatar3.jpg"
 const img3 = "img/avatar4.jpg"
 const img4 = "img/avatar5.jpg"
 
+// review data
+const reviews = [
+  {
+    id: 1,
+    name: "Elizabeth Smith",
+    job: "UX designer",
+    img: {img1},
+    text: "Impression up admiration he by partiality is. Instantly immediate his saw one day perceived. Old blushes respect but offices hearted minutes effects. ",
+  },
+
+  {
+    id: 2,
+    name: "John Doe",
+    job: "Web Developer",
+    img: {img3},
+    text: "Dependent on so extremely delivered by. Yet no jokes worse her why. Bed one supposing breakfast day fulfilled off depending questions. Whatever boy her exertion his extended. Ecstatic followed handsome drawings entirely mrs one yet outweigh",
+  },
+
+  {
+    id: 3,
+    name: "Kristen Rovers",
+    job: "Web Designer",
+    img: {img4},
+    text: "So feel been kept be at gate. Be september it extensive oh concluded of certainty. In read most gate at body held it ever no. Talking justice welcome message inquiry in started of am me.",
+  },
+
+  {
+    id: 4,
+    name: "Matt Downey",
+    job: "Data Analyst",
+    img: {img5},
+    text: "Occasional middletons everything so to. Have spot part for his quit may. Enable it is square my an regard. Often merit stuff first oh up hills as he.",
+  },
+];
+
+
